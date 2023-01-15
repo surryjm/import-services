dec3d889c974d4163a6988b88c4f84b19899b1db
Surry Mowery <surrykj@gmail.com>
2023-01-15 12:57:50 -0800

    added error handling for read_rows and import_services

--- create_services.py
+++ create_services.py
@@ -1,9 +1,22 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+import sys
 
 import requests
 import chardet
 import argparse
 import csv
+
+
+def exit_program():
+    """ Exit program """
+    print("Goodbye!")
+    sys.exit()
+
+
+def ask_question(prompt):
+    """ One function for user input """
+    answer = input(prompt)
+    return answer
 
 
 def read_rows(args):
@@ -14,12 +27,52 @@ def read_rows(args):
     with open(filename, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            service_dict = {"service": {"type": row["type"], "name": row["name"], "description": row["description"],
-                                        "auto_resolve_timeout": int(row["auto_resolve_timeout"]),
-                                        "escalation_policy": {"id": row["escalation_policy.id"],
-                                                              "type": row["escalation_policy.type"]}}}
-            file_data.append(service_dict)
-    print(file_data)
+            if not row["name"]:
+                while True:
+                    user_input = ask_question(f"Regarding row: \n{row}\nName is blank. This will throw a "
+                                              f"400 error and row will not be imported. Continue? [Y/N] --> ")
+                    answer = user_input.upper()
+                    if answer == "Y":
+                        print(f"Your response: {answer}\n")
+                        break
+                    elif answer == "N":
+                        print(f"Your response: {answer}")
+                        exit_program()
+                    else:
+                        print("\nInvalid response, please try again\n")
+            elif not row["description"]:
+                while True:
+                    user_input = ask_question(f"Regarding row: \n{row}\nDescription is blank. Continue? [Y/N] --> ")
+                    answer = user_input.upper()
+                    if answer == "Y":
+                        print(f"Your response: {answer}\n")
+                        break
+                    elif answer == "N":
+                        print(f"Your response: {answer}")
+                        exit_program()
+                    else:
+                        print("\nInvalid response, please try again\n")
+            elif not row["auto_resolve_timeout"]:
+                while True:
+                    user_input = ask_question(f"Regarding row: \n{row}\nAuto resolve timeout is blank. "
+                                              f"Continue? [Y/N] --> ")
+                    answer = user_input.upper()
+                    if answer == "Y":
+                        print(f"Your response: {answer}\n")
+                        break
+                    elif answer == "N":
+                        print(f"Your response: {answer}")
+                        exit_program()
+                    else:
+                        print("\nInvalid response, please try again\n")
+            try:
+                service_dict = {"service": {"type": row["type"], "name": row["name"], "description": row["description"],
+                                            "auto_resolve_timeout": row["auto_resolve_timeout"],
+                                            "escalation_policy": {"id": row["escalation_policy.id"],
+                                                                  "type": row["escalation_policy.type"]}}}
+                file_data.append(service_dict)
+            except KeyError as err:
+                print(f'Missing column in file: {err}')
     import_services(key, file_data)
 
 
@@ -31,14 +84,43 @@ def import_services(key, file_data):
                'Authorization': f'Token token={key}'}
     for row in file_data:
         payload = row
-        print(payload)
-        response = requests.post(url, json=payload, headers=headers)
-        print(f'Statuscode: {response.status_code}')
-        response_dict = response.json()
-        print(response_dict.keys())
+        try:
+            response = requests.post(url, json=payload, headers=headers)
+            if response.status_code == 201:
+                print(f'Statuscode: {response.status_code}, the following was imported successfully: \n{payload}\n')
+            response_dict = response.json()
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            if response.status_code == 400:
+                print(f'Statuscode: {response.status_code}, the following was NOT imported: \n{payload}\n'
+                      f'Caller provided invalid arguments. Please review the response for error details. '
+                      f'Retrying with the same arguments will not work, {err}\n')
+            elif response.status_code == 401:
+                print(f'Statuscode: {response.status_code}, the following was NOT imported: \n{payload}\n'
+                      f'Caller did not supply credentials or did not provide the correct credentials. '
+                      f'If you are using an API key, it may be invalid or your Authorization header '
+                      f'may be malformed, {err}\n')
+            elif response.status_code == 402:
+                print(f'Statuscode: {response.status_code}, the following was NOT imported: \n{payload}\n'
+                      f'Account does not have the abilities to perform the action. '
+                      f'Please review the response for the required abilities. '
+                      f'You can also use the Abilities API to determine what features are available to your account, '
+                      f'{err}\n')
+            elif response.status_code == 403:
+                print(f'Statuscode: {response.status_code}, the following was NOT imported: \n{payload}\n'
+                      f'Caller is not authorized to view the requested resource. '
+                      f'While your authentication is valid, the authenticated user or token does not have '
+                      f'permission to perform this action, {err}\n')
+        except requests.exceptions.ConnectionError as err:
+            print(f'A Connection error occurred, {err}')
+        except requests.exceptions.Timeout as err:
+            print(f'The request timed out, {err}')
+        except requests.exceptions.RequestException as err:
+            print(f'There was an ambiguous exception that occurred while handling your request, {err}')
 
 
 def main():
+    """ Grab the API key and file name from script execution """
     parser = argparse.ArgumentParser(description='Bulk import of services from CSV file')
     parser.add_argument('-k', '--api-key', required=True, help='Global REST API key')
     parser.add_argument('-f', '--filename', required=True, help='CSV file of services to import')
